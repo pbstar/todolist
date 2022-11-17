12caff2b0b7321561b00c032ea01be3591eddca2
pengbo <pengbo@hopefound.cn>
2022-11-17 09:41:11 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -2,148 +2,76 @@
   <div id="app">
     <div class="top">
       <div class="topleft">
-        <van-button
-          type="info"
-          :plain="type != 0"
-          @click="type = 0"
-          size="small"
-          >全部</van-button
-        >
-        <van-button
-          type="info"
-          :plain="type != 1"
-          @click="type = 1"
-          size="small"
-          >未开始</van-button
-        >
-        <van-button
-          type="info"
-          :plain="type != 2"
-          @click="type = 2"
-          size="small"
-          >进行中</van-button
-        >
-        <van-button
-          type="info"
-          :plain="type != 3"
-          @click="type = 3"
-          size="small"
-          style="border-right: 1px solid #1989fa"
-          >已完成
+        <van-button type="info" :plain="type != 0" @click="type = 0" size="small">全部</van-button>
+        <van-button type="info" :plain="type != 1" @click="type = 1" size="small">未开始</van-button>
+        <van-button type="info" :plain="type != 2" @click="type = 2" size="small">进行中</van-button>
+        <van-button type="info" :plain="type != 3" @click="type = 3" size="small"
+          style="border-right: 1px solid #1989fa">已完成
         </van-button>
       </div>
       <van-button type="info" size="small" @click="isDate = true">
         <span>{{ date }}</span>
         <van-icon name="arrow-down" />
       </van-button>
-      <van-calendar
-        v-model="isDate"
-        :min-date="minDate"
-        :show-confirm="false"
-        @confirm="toDate"
-      />
+      <van-calendar v-model="isDate" :min-date="minDate" :show-confirm="false" @confirm="toDate" />
     </div>
     <div class="mid">
       <div v-for="(item, index) in list" :key="index">
-        <van-cell
-          :title="
-            index +
-            1 +
-            '. [' +
-            (item.type == 3 ? '已完成' : item.type == 2 ? '进行中' : '未开始') +
-            '] ' +
-            item.name
-          "
-          v-show="item.type == type || type == 0"
-          :title-style="
-            item.priority == 1
-              ? 'color:red'
-              : item.priority == 2
-              ? 'color:orange'
-              : item.priority == 3
-              ? 'color:blue'
-              : 'color:green'
-          "
-          @click="toUpdate(item)"
-        >
+        <van-cell :title="
+          index +
+          1 +
+          '. [' +
+          (item.type == 3 ? '已完成' : item.type == 2 ? '进行中' : '未开始') +
+          '] ' +
+          item.name + item.priority
+        " v-show="item.type == type || type == 0" :title-style="
+  item.priority == 1
+    ? 'color:red'
+    : item.priority == 2
+      ? 'color:orange'
+      : item.priority == 3
+        ? 'color:blue'
+        : 'color:green'
+" @click="toUpdate(item)">
           <van-icon name="edit" size="20" />
         </van-cell>
       </div>
 
       <van-empty description="暂无待办事项" v-show="list.length == 0" />
     </div>
-    <van-icon
-      name="add"
-      id="add"
-      size="50"
-      color="#1989fa"
-      @click="isAdd = true"
-    />
+    <van-icon name="add" id="add" size="50" color="#1989fa" @click="isAdd = true" />
     <van-popup v-model="isAdd" round closeable position="bottom" class="addBox">
       <p class="atitle">添加待办事项</p>
       <van-form @submit="toAdd">
-        <van-field
-          v-model="name"
-          required
-          autofocus
-          label="待办事项"
-          label-width="4.3em"
-          placeholder="请输入待办事项"
-        />
+        <van-field v-model="name" required autofocus label="待办事项" label-width="4.3em" placeholder="请输入待办事项" />
         <van-field name="radio" label="优先级" required label-width="4.3em">
           <template #input>
             <van-radio-group v-model="priority" direction="horizontal">
               <van-radio name="1">重要且紧急</van-radio>
               <van-radio name="2">紧急不重要</van-radio>
               <van-radio name="3" style="margin-top: 5px">重要不紧急</van-radio>
-              <van-radio name="4" style="margin-top: 5px"
-                >不紧急不重要</van-radio
-              >
+              <van-radio name="4" style="margin-top: 5px">不紧急不重要</van-radio>
             </van-radio-group>
           </template>
         </van-field>
-        <van-field
-          v-model="message"
-          rows="2"
-          autosize
-          label="备注"
-          type="textarea"
-          label-width="4.3em"
-          placeholder="请输入备注"
-        />
+        <van-field v-model="message" rows="2" autosize label="备注" type="textarea" label-width="4.3em"
+          placeholder="请输入备注" />
         <div style="margin: 10px 16px 16px">
-          <van-button round block type="info" native-type="submit"
-            >提交</van-button
-          >
+          <van-button round block type="info" native-type="submit">提交</van-button>
         </div>
       </van-form>
     </van-popup>
-    <van-popup
-      v-model="isUpdate"
-      round
-      closeable
-      position="bottom"
-      style="height: 342px"
-      class="addBox"
-    >
+    <van-popup v-model="isUpdate" round closeable position="bottom" style="height: 342px" class="addBox">
       <p class="atitle">编辑待办事项</p>
       <van-form @submit="sendUpdate">
-        <van-field
-          v-model="upObj.name"
-          required
-          label="待办事项"
-          label-width="4.3em"
-          placeholder="请输入待办事项"
-        />
+        <van-field v-model="upObj.name" required label="待办事项" label-width="4.3em" placeholder="请输入待办事项" />
         <van-field name="radio" label="优先级" required label-width="4.3em">
           <template #input>
             <van-radio-group v-model="upObj.priority" direction="horizontal">
               <van-radio name="1">重要且紧急</van-radio>
               <van-radio name="2">紧急不重要</van-radio>
               <van-radio name="3" style="margin-top: 5px">重要不紧急</van-radio>
-              <van-radio name="4" style="margin-top: 5px"
-                >不紧急不重要</van-radio
-              >
+              <van-radio name="4" style="margin-top: 5px">不紧急不重要</van-radio>
             </van-radio-group>
           </template>
         </van-field>
@@ -156,19 +84,10 @@
             </van-radio-group>
           </template>
         </van-field>
-        <van-field
-          v-model="upObj.message"
-          rows="2"
-          autosize
-          label="备注"
-          type="textarea"
-          label-width="4.3em"
-          placeholder="请输入备注"
-        />
+        <van-field v-model="upObj.message" rows="2" autosize label="备注" type="textarea" label-width="4.3em"
+          placeholder="请输入备注" />
         <div style="margin: 10px 16px 16px">
-          <van-button round block type="info" native-type="submit"
-            >提交</van-button
-          >
+          <van-button round block type="info" native-type="submit">提交</van-button>
         </div>
       </van-form>
     </van-popup>
@@ -287,7 +206,7 @@ export default {
 .top {
   display: flex;
   justify-content: space-between;
-  margin: 6px 6px 6px 5px;
+  margin: 10px 9px 10px 8px;
 }
 
 .top .van-button--small {
@@ -307,23 +226,27 @@ export default {
 
 .mid {
   width: 100%;
-  height: 612px;
+  height: 592px;
   overflow-y: auto;
 }
+
 .mid .van-cell {
-  padding: 5px 6px;
+  padding: 5px 9px;
 }
+
 .mid .van-cell__title {
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
   min-width: 335px;
 }
+
 .mid .van-cell__value {
   display: flex;
   justify-content: flex-end;
   align-items: center;
 }
+
 .addBox {
   height: 300px;
 }
